c47b1408a54f66e159cdef6d4ee2bc881be93044
Youssef mohammed <ym99160@gmail.com>
2024-12-15 22:05:20 +0200

    change theme and creat note view

---
+++ lib/views/note_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class note_view extends StatelessWidget {
+  const note_view({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
