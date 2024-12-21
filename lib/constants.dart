cea07727ace1119b457d2231409dbe8c49b0808b
Youssef mohammed <ym99160@gmail.com>
2024-12-21 08:22:54 +0200

    init hive

--- lib/constants.dart
+++ lib/constants.dart
@@ -2,3 +2,4 @@ import 'package:flutter/material.dart';
 
 const  kPrimarycolor  = Color(0xff62FCD7);
 const kSecondarycolor = Colors.blueGrey;
+const  kNoteBox = "note_box";
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/adapters.dart';
+import 'package:note_app/constants.dart';
 import 'package:note_app/views/note_view.dart';
 
-void main() {
+void main() async {
+ await Hive.initFlutter();
+ 
+ await Hive.openBox(kNoteBox);
+
   runApp(const NoteApp());
 }
-
 class NoteApp extends StatelessWidget {
   const NoteApp({super.key});
 
