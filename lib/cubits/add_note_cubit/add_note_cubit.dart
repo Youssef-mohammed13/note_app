1c75333a72d45b06beefb58ceb19119165be1f10
Youssef mohammed <ym99160@gmail.com>
2024-12-22 11:11:56 +0200

    provide add note cubit

--- lib/cubits/add_note_cubit/add_note_cubit.dart
+++ lib/cubits/add_note_cubit/add_note_cubit.dart
@@ -1,10 +1,25 @@
-import 'package:bloc/bloc.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hive_flutter/adapters.dart';
+import 'package:note_app/constants.dart';
 import 'package:note_app/models/note_model.dart';
+
 part 'add_note_state.dart';
 
 class AddNoteCubit extends Cubit<AddNoteState> {
   AddNoteCubit() : super(AddNoteInitial());
 
+ addNote({required NoteModel note}){
 
+  emit(AddNoteLoadig());
+  try{
+   var notesBox= Hive.box<NoteModel>(kNoteBox);
+   emit(AddNoteSuccess());
+   notesBox.add(note);
+  }catch(e)
+  { AddNoteFaliure(errorMessage: " error ___ ${e.toString()}");
+  
+  }
+
+ }
 
 } 
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/adapters.dart';
 import 'package:note_app/constants.dart';
+import 'package:note_app/cubits/add_note_cubit/add_note_cubit.dart';
 import 'package:note_app/models/note_model.dart';
 import 'package:note_app/views/note_view.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() async {
  await Hive.initFlutter();
@@ -13,17 +15,21 @@ void main() async {
 }
 class NoteApp extends StatelessWidget {
   const NoteApp({super.key});
-
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        brightness: Brightness.dark,
-        fontFamily: "Poppins" ,
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (context) =>  AddNoteCubit(),),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          brightness: Brightness.dark,
+          fontFamily: "Poppins" ,
+        ),
+        home: const note_view(),
       ),
-      home: const note_view(),
     );
   }
 }
