3bbf90f011d9dbff62b4c4d875ae9facb883ad59
Youssef mohammed <ym99160@gmail.com>
2024-12-23 16:46:31 +0200

    refactor bloc providir

--- lib/views/widgets/NoteBottomSheetBody.dart
+++ lib/views/widgets/NoteBottomSheetBody.dart
@@ -9,23 +9,26 @@ class myModalBottomSheetBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 16),
-      child: BlocConsumer<AddNoteCubit, AddNoteState>(
-        listener: (context, state) {
-          if (state is AddNoteFaliure) {
-            print("failed ${state.errorMessage}");
-          }
-          if (state is AddNoteSuccess) {
-            // Navigator.pop(context);
-            const SnackBar(content: Text("Success"));
-          }
-        },
-        builder: (context, state) {
-          return ModalProgressHUD(
-              inAsyncCall: state is AddNoteLoadig ? true : false,
-              child: const AddNoteForm());
-        },
+    return BlocProvider(
+      create: (context) => AddNoteCubit(),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16),
+        child: BlocConsumer<AddNoteCubit, AddNoteState>(
+          listener: (context, state) {
+            if (state is AddNoteFaliure) {
+              print("failed ${state.errorMessage}");
+            }
+            if (state is AddNoteSuccess) {
+              // Navigator.pop(context);
+              const SnackBar(content: Text("Success"));
+            }
+          },
+          builder: (context, state) {
+            return ModalProgressHUD(
+                inAsyncCall: state is AddNoteLoadig ? true : false,
+                child: const AddNoteForm());
+          },
+        ),
       ),
     );
   }
